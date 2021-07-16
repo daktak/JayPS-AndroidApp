969381305b240474b400c7340d26293791a50cf8
daktak <daktak@domo.ddns.net>
2021-07-16 18:33:57 +1000

    allow no lights

--- app/src/main/java/com/njackson/sensor/Ble.java
+++ app/src/main/java/com/njackson/sensor/Ble.java
@@ -387,12 +387,14 @@ public class Ble implements IBle, ITimerHandler {
 	    final BluetoothGattService gattService = gatt.getService(UUID_LIGHT_MODE_SERVICE);
 	    if (gattService == null) {
 		   Log.i(TAG, "LIGHT MODE SERVICE not found");
+	           continueWrite(gatt);
 		   return;
 	    }
 		    
 	    final BluetoothGattCharacteristic gattChar = gattService.getCharacteristic(UUID_LIGHT_MODE);
 	    if (gattChar == null) {
 		   Log.i(TAG, "LIGHT MODE not found");
+	           continueWrite(gatt);
 		   return;
 	    }
 	    if ((newMode==0)||(light_mode==0)) {
@@ -400,6 +402,10 @@ public class Ble implements IBle, ITimerHandler {
 		    gattChar.setValue(newMode, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
 		    gatt.writeCharacteristic(gattChar);
 	    }
+	    continueWrite(gatt);
+    }
+
+    public void continueWrite(BluetoothGatt gatt) {
 	//moved this to here so the lightMode can be set without having to create a characteristicWriteQueue
         Log.d(TAG, "descriptorWriteQueue.size=" + descriptorWriteQueue.size());
         if (descriptorWriteQueue.size() > 0) {
