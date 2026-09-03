7c561d14fc7757326849569b5a1270d2d60044c8
daktak <daktak@domo.ddns.net>
2026-09-03 15:37:39 +1000

    osm map

--- app/src/main/java/com/njackson/ui/dashboard/MapCard.kt
+++ app/src/main/java/com/njackson/ui/dashboard/MapCard.kt
@@ -57,7 +57,7 @@ fun MapCard(trail: List<TrailPoint>, isIndoor: Boolean = false, modifier: Modifi
     }
     val map = remember {
         MapView(ctx).apply {
-            setTileSource(TileSourceFactory.CYCLE)
+            setTileSource(TileSourceFactory.HIKEBIKEMAP)
             setMultiTouchControls(true)
             controller.setZoom(15.0)
             isTilesScaledToDpi = true
